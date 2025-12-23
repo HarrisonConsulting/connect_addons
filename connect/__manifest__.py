028666a8104ddd86659ceff8cd951200c2419f33
Matt Harrison <matt@harrison.consulting>
2025-12-23 14:41:18 -0500

    fix: Update version number to 1.0.21 in manifest and enhance styling for phone components

--- connect/__manifest__.py
+++ connect/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Connect',
-    'version': '1.0.20',
+    'version': '1.0.21',
     'author': 'Oduist',
     'maintainer': 'Oduist',
     'live_test_url': 'https://connect-demo-18.oduist.com/',

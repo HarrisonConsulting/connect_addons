da77b846ddec69ae08d54fb41489308e9b3f3dc9
Matt Harrison <matt@harrison.consulting>
2026-01-17 07:53:50 -0600

    chore: bump versions for Odoo 19 migration
    - connect: 1.0.25 -> 1.0.31
    - connect_crm: 1.0.3 -> 1.0.4
    - connect_elevenlabs: 1.4.0 -> 1.4.2

--- connect/__manifest__.py
+++ connect/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Connect',
-    'version': '1.0.25',
+    'version': '1.0.31',
     'author': 'Oduist',
     'maintainer': 'Oduist',
     'live_test_url': 'https://connect-demo-18.oduist.com/',
--- connect_elevenlabs/__manifest__.py
+++ connect_elevenlabs/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Connect ElevenLabs',
-    'version': '1.4.0',
+    'version': '1.4.2',
     'author': 'Oduist',
     'price': 0,
     'currency': 'EUR',

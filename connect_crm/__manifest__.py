5c8203584e149647660b122df53b2e78a679f35e
Matt Harrison <matt@harrison.consulting>
2026-05-15 13:25:46 -0500

    Bump version to 1.0.7 in __manifest__.py

--- connect_crm/__manifest__.py
+++ connect_crm/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 {
     'name': 'Twilio Odoo CRM integration',
-    'version': '1.0.6',
+    'version': '1.0.7',
     'author': 'Oduist',
     'price': 0,
     'currency': 'EUR',

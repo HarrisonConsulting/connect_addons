0ce3887d6e4a5cf2bbdcfada7532639b2dce9f8a
Matt Harrison <matt@harrison.consulting>
2026-08-23 02:28:11 -0500

    feat(connect): update version to 1.30.2 and enhance tray badge positioning

--- connect/__manifest__.py
+++ connect/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Connect',
-    'version': '1.30.1',
+    'version': '1.30.2',
     'author': 'Harrison Consulting, LLC',
     'website': 'https://www.harrison.consulting',
     'maintainer': 'Oduist',
